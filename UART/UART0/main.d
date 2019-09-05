94a67c0fd3ed2e8753447e09238aeed0638ef3a5
George <gcdagis97@gmail.com>
2019-09-05 12:09:28 -0400

    Add files via upload

---
+++ UART/UART0/main.d
@@ -0,0 +1,8 @@
+.\main.o: main.c
+.\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\main.o: C:\Keil_v5\ARM\INC\TI\TM4C123\tm4c123gh6pm.h
+.\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm4.h
+.\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
+.\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
+.\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm4_simd.h
+.\main.o: C:\Keil_v5\ARM\INC\TI\TM4C123\system_TM4C123.h
